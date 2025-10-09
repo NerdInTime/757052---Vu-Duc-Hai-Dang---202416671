2e956435a09c05c4062390b0f44bdd2500a2ccec
NerdInTime <Dang.VDH2416671@sis.hust.edu.vn>
2025-10-09 13:48:40 +0700

    first save step

--- Lab 02/AimsProject/src/aims/DigitalVideoDisc.java
+++ Lab 02/AimsProject/src/aims/DigitalVideoDisc.java
@@ -6,5 +6,45 @@ public class DigitalVideoDisc {
 	private String director;
 	private int length;
 	private float cost;
+	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.length = length;
+		this.cost = cost;
+	}
+	public DigitalVideoDisc(String director, String category, String title, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.cost = cost;
+	}
+	public DigitalVideoDisc(String category, String title, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+	}
+	public DigitalVideoDisc(String title) {
+		super();
+		this.title = title;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public int getLength() {
+		return length;
+	}
+	public float getCost() {
+		return cost;
+	}
 
 }
