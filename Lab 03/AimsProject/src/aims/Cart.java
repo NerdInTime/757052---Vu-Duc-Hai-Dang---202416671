7a3ef2d6e88f58ceba0caef1f4d060c6582a1397
NerdInTime <Dang.VDH2416671@sis.hust.edu.vn>
2025-10-23 13:21:48 +0700

    method overloading done i htink

--- Lab 03/AimsProject/src/aims/Cart.java
+++ Lab 03/AimsProject/src/aims/Cart.java
@@ -17,6 +17,51 @@ public class Cart {
 			}
 		}
 	}
+	public void addDigitalVideoDisc(DigitalVideoDisc [] dvdList) {
+		if (qtyOrdered>=MAX_NUMBERS_ORDERED) {
+			System.out.println("Full cart, buddy. Hold off on your greed.");
+		}
+		else {
+			for (DigitalVideoDisc i: dvdList) {
+				if (qtyOrdered>=MAX_NUMBERS_ORDERED) {
+					System.out.println("Full cart, buddy. Hold off on your greed.");
+					break;
+				}
+				else {
+					itemsOrdered[qtyOrdered]=i;
+					qtyOrdered++;
+					System.out.println("Disc " + i.getTitle() + " has been added to your cart.");
+					if (qtyOrdered>=MAX_NUMBERS_ORDERED-5) {
+						System.out.println("Woah pardner. Disc " + i.getTitle() + " has been added but..." + (MAX_NUMBERS_ORDERED-qtyOrdered) + " more discs and you're full. Watch out.");
+					}
+				}
+			}
+		}
+	}
+	public void addDigitalVideoDisc(DigitalVideoDisc disc1, DigitalVideoDisc disc2) {
+		if (qtyOrdered>=MAX_NUMBERS_ORDERED) {
+			System.out.println("Full cart, buddy. Hold off on your greed.");
+		}
+		else {
+			itemsOrdered[qtyOrdered]=disc1;
+			qtyOrdered++;
+			System.out.println("Disc " + disc1.getTitle() + " has been added to your cart.");
+			if (qtyOrdered>=MAX_NUMBERS_ORDERED-5) {
+				System.out.println("Woah pardner. Disc " + disc1.getTitle() + " has been added but..." + (MAX_NUMBERS_ORDERED-qtyOrdered) + " more discs and you're full. Watch out.");
+			}		
+		}
+		if (qtyOrdered>=MAX_NUMBERS_ORDERED) {
+			System.out.println("Full cart, buddy. Hold off on your greed.");
+		}
+		else {
+			itemsOrdered[qtyOrdered]=disc2;
+			qtyOrdered++;
+			System.out.println("Disc " + disc2.getTitle() + " has been added to your cart.");
+			if (qtyOrdered>=MAX_NUMBERS_ORDERED-5) {
+				System.out.println("Woah pardner. Disc " + disc2.getTitle() + " has been added but..." + (MAX_NUMBERS_ORDERED-qtyOrdered) + " more discs and you're full. Watch out.");
+			}		
+		}
+	}
     public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
         boolean found = false;
         for (int i = 0; i < qtyOrdered; i++) {
