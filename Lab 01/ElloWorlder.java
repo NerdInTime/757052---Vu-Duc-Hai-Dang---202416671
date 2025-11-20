9de9a115dc6cff93727261088f8746b52e49125d
NerdInTime <Dang.VDH2416671@sis.hust.edu.vn>
2025-11-20 09:44:22 +0700

    initializing lab 4

--- Lab 01/ElloWorlder.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-public class FirstDialog {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null,"Hello World, I am Jo");
-        System.exit(0);
-    }
-}
